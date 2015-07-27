2f21af894ed9e1bb10c7a03370a47dbdb953cf02
ErnestX <ernestxiangjlnq2011@gmail.com>
2015-07-27 16:50:58 -0700

    encapsulated rangeleft and rangeright into bar manager

--- BarManager.js
+++ BarManager.js
@@ -1,10 +1,18 @@
 "use strict";
 
 var barManager = {
+    bar: null, 
     rangeLeft: 0, 
-    rangeRight: 9, 
-    initBar: function(b) {
+    rangeRight: 0, 
+    
+    /*
+    init this.bar, this.rangeLeft, this.rangeRight and place the plans
+    */
+    initWithBarAndRange: function(b, l, r) {
         this.bar = b;
+        this.rangeLeft = l;
+        this.rangeRight = r;
+        
         var i;
         for (i=this.rangeLeft; i < this.rangeRight; i++) {
             var img = getPlanAtIndex(i);
@@ -33,7 +41,6 @@ var barManager = {
     moveBarLeftByOnePlan: function() {
         this.bar.addClass("leftShiftting");
         this.bar.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
-//            console.log(this.deletePlanAtLeft);
             barManager.deletePlanAtLeft();
             barManager.addNewPlanAtRight();
             barManager.bar.removeClass("leftShiftting");
@@ -45,7 +52,7 @@ var barManager = {
     moveBarRightByOnePlan: function() {
         this.bar.addClass("rightShiftting");
         this.bar.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
-            barManager.deletePlanAtRight();
+            barManager.deletePlanAtRight(); //"this" no longer works here
             barManager.addNewPlanAtLeft();
             barManager.bar.removeClass("rightShiftting");
         });   
@@ -72,70 +79,3 @@ var barManager = {
         this.rangeRight--;
     }
 };
-//
-///*
-//does not move the bar
-//*/
-//BarManager.prototype.selectIndex = function(planIndex) 
-//{
-//    // unselect all plans on screen
-//    this.bar.children().removeClass("selected");
-//    this.bar.children().addClass("unselected");
-//    // select plan
-//    this.bar.children().eq(planIndex - this.rangeLeft).removeClass("unselected");
-//    this.bar.children().eq(planIndex - this.rangeLeft).addClass("selected");
-//            
-//    console.log("select #" + planIndex.toString());
-//}
-//
-///*
-//moves the bar with animation. Adds and deletes plans so that the number of plans on screen stays the same
-//*/
-//BarManager.prototype.moveBarLeftByOnePlan = function()
-//{
-//    this.bar.addClass("leftShiftting");
-//    this.bar.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
-//        console.log(this.deletePlanAtLeft);
-//        this.deletePlanAtLeft();
-//        this.addNewPlanAtRight();
-//        this.bar.removeClass("leftShiftting");
-//    });
-//}
-//
-///*
-//moves the bar with animation. Adds and deletes plans so that the number of plans on screen stays the same
-//*/
-//BarManager.prototype.moveBarRightByOnePlan = function()
-//{
-//    this.bar.addClass("rightShiftting");
-//    this.bar.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
-//        BarManager.prototype.deletePlanAtRight();
-//        BarManager.prototype.addNewPlanAtLeft();
-//        this.bar.removeClass("rightShiftting");
-//    });   
-//}
-//
-//BarManager.prototype.addNewPlanAtLeft = function()
-//{
-//    var newImg = getPlanAtIndex(this.rangeLeft - 1);
-//    initPlan(newImg);
-//    this.bar.prepend(newImg);
-//    this.rangeLeft--;
-//}
-//
-//BarManager.prototype.addNewPlanAtRight = function() {
-//    var newImg = getPlanAtIndex(this.rangeRight + 1);
-//    initPlan(newImg);
-//    this.bar.append(newImg);
-//    this.rangeRight++;
-//}
-//
-//BarManager.prototype.deletePlanAtLeft = function() {
-//    this.bar.children().eq(0).remove();
-//    this.rangeLeft++;
-//}
-//
-//BarManager.prototype.deletePlanAtRight = function() {
-//    this.bar.children().last().remove();
-//    this.rangeRight--;
-//}
