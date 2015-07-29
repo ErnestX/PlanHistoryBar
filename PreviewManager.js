baf40b441efd85de58a1c5561836157972cefe6b
ErnestX <ernestxiangjlnq2011@gmail.com>
2015-07-29 16:48:00 -0700

    improved visual

--- PreviewManager.js
+++ PreviewManager.js
@@ -2,6 +2,7 @@ MY_GLOBAL.previewManager = {
     window: null, 
     initWithWindowAndIndex: function(w, i) {
         MY_GLOBAL.typeChecker.assertIsObject(w);
+        MY_GLOBAL.typeChecker.assertIsInteger(i);
         
         this.window = w;
         var newPlan = MY_GLOBAL.dataManager.getPlanAtIndex(i);
