f5ff6da67ec1ada1ebd392d0597d6a4c843e5990
ErnestX <ernestxiangjlnq2011@gmail.com>
2015-07-31 14:17:29 -0700

    implemented using two images, yet there are css clip lag

--- PreviewWindowManager.js
+++ PreviewWindowManager.js
@@ -2,50 +2,61 @@
 
 MY_GLOBAL.previewWindowManager = {
     previewWindow: null, 
+    frontImage: null,
+    backImage:null,
     initWithPreviewWindowAndIndex: function(w, i) {
         MY_GLOBAL.typeChecker.assertIsObject(w);
         MY_GLOBAL.typeChecker.assertIsInteger(i);
         
         this.previewWindow = w;
-        this.previewWindow.css('height', '550px');
+        
+        // create a container with two imgs
+        this.frontImage = this.createPlaceHolderImage();
+        this.backImage = this.createPlaceHolderImage();
+//        console.log(this.backImage);
+        this.previewWindow.append(this.backImage);
+        this.previewWindow.append(this.frontImage);
+        
+//        console.log(this.previewWindow.children());
+        
         this.showPreviewWindowOfIndex(i);
     }, 
     
     showPreviewWindowOfIndex: function(i) {
         var newPlan = MY_GLOBAL.dataManager.getPlanAtIndex(i);
-//        this.previewWindow.addClass("fadingOut");
-//        this.previewWindow.css('background-image', 'url(/' + newPlan.previewWindowSrc + ')');
         
-        var newPreviewImage = this.renderPreviewWindowImageFromSource(newPlan.previewWindowSrc);
-        this.previewWindow.children().remove(); //NOTE: removes all the children of the previewWindow
-        this.previewWindow.append(newPreviewImage);
-        newPreviewImage.classList.add("fadingIn");
-        this.previewWindow.one('animationend', function() {
-//            MY_GLOBAL.previewWindowManager.previewWindow.children().remove(); //NOTE: removes all the children of the previewWindow
-//            MY_GLOBAL.previewWindowManager.previewWindow.append(MY_GLOBAL.previewWindowManager.renderPreviewWindowImageFromSource(newPlan.previewWindowSrc));
-//            console.log(newPlan.previewWindowSrc);
-//            MY_GLOBAL.previewWindowManager.previewWindow.css('background-image', 'url(/' + newPlan.previewWindowSrc + ')');
-//            MY_GLOBAL.previewWindowManager.previewWindow.addClass('fadingIn');
-//            MY_GLOBAL.previewWindowManager.previewWindow.removeClass('fadingOut');
-//            MY_GLOBAL.previewWindowManager.previewWindow.one('animationend', function() {
-//                MY_GLOBAL.previewWindowManager.previewWindow.removeClass('fadingIn');
-//            });
-            
-            
-//-----------------------------------------------------
+        // Step1: change front img src and fadein
+        this.frontImage.addClass('hidden');
+        this.frontImage.attr('src', newPlan.previewWindowSrc);
+        this.frontImage.removeClass('hidden');
+        this.frontImage.addClass('fadingIn');
+        
+//        var newPreviewImage = this.renderPreviewWindowImageFromSource(newPlan.previewWindowSrc);
+//        this.previewWindow.append(newPreviewImage);
+//        newPreviewImage.classList.add("fadingIn");
+        
+        this.frontImage.one('animationend', function() {
+            // Step3: remove css animation and update the underlying image
             MY_GLOBAL.
             previewWindowManager.
-            previewWindow.
-            css('background-image', 'url(/' + newPlan.previewWindowSrc + ')');
+            frontImage.removeClass('fadingIn');
+            MY_GLOBAL.
+            previewWindowManager.
+            backImage.attr('src', newPlan.previewWindowSrc);
         });
     }, 
-    
-    renderPreviewWindowImageFromSource: function(source) {
-        MY_GLOBAL.typeChecker.assertIsString(source);
-        
-        var image = new Image();
-        image.src = source;
-        image.classList.add("previewWindowImage");
+    createPlaceHolderImage: function() {
+        var image = $('<img>');
+        image.addClass("previewWindowImage");
         return image;
-    }
+    },
+    
+//    renderPreviewWindowImageFromSource: function(source) {
+//        MY_GLOBAL.typeChecker.assertIsString(source);
+//        
+//        var image = new Image();
+//        image.src = source;
+//        image.classList.add("previewWindowImage");
+//        return image;
+//    }
 }
