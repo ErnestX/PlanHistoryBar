ce073b7d716b890e366dcd0da295f023aa502de7
ErnestX <ernestxiangjlnq2011@gmail.com>
2015-07-31 14:53:31 -0700

    cleaned up. Now the preview window can be resized dynamically. Cross fade effect finished

--- PreviewWindowManager.js
+++ PreviewWindowManager.js
@@ -9,16 +9,14 @@ MY_GLOBAL.previewWindowManager = {
         MY_GLOBAL.typeChecker.assertIsInteger(i);
         
         this.previewWindow = w;
-//        this.previewWindow.css('height', '700px');
+        /*NOTE: can dynamically adjust image size by setting window height, 
+        e.g. this.previewWindow.css('height', '700px'); */
         
-        // create a container with two imgs
+        // create two imgs place holders
         this.frontImage = this.createPlaceHolderImage();
         this.backImage = this.createPlaceHolderImage();
-//        console.log(this.backImage);
         this.previewWindow.append(this.backImage);
         this.previewWindow.append(this.frontImage);
-        
-//        console.log(this.previewWindow.children());
         
         this.showPreviewWindowOfIndex(i);
     }, 
@@ -26,21 +24,18 @@ MY_GLOBAL.previewWindowManager = {
     showPreviewWindowOfIndex: function(i) {
         var newPlan = MY_GLOBAL.dataManager.getPlanAtIndex(i);
         
-        // Step1: change front img src and fadein
+        // Step1: update front img src and fadein
         this.frontImage.addClass('hidden');
         this.frontImage.attr('src', newPlan.previewWindowSrc);
         this.frontImage.removeClass('hidden');
         this.frontImage.addClass('fadingIn');
         
-//        var newPreviewImage = this.renderPreviewWindowImageFromSource(newPlan.previewWindowSrc);
-//        this.previewWindow.append(newPreviewImage);
-//        newPreviewImage.classList.add("fadingIn");
-        
+        // Step2: once finished fading in, remove css animation and update the back image
         this.frontImage.one('animationend', function() {
-            // Step3: remove css animation and update the underlying image
             MY_GLOBAL.
             previewWindowManager.
             frontImage.removeClass('fadingIn');
+            
             MY_GLOBAL.
             previewWindowManager.
             backImage.attr('src', newPlan.previewWindowSrc);
@@ -51,13 +46,4 @@ MY_GLOBAL.previewWindowManager = {
         image.addClass("previewWindowImage");
         return image;
     },
-    
-//    renderPreviewWindowImageFromSource: function(source) {
-//        MY_GLOBAL.typeChecker.assertIsString(source);
-//        
-//        var image = new Image();
-//        image.src = source;
-//        image.classList.add("previewWindowImage");
-//        return image;
-//    }
 }
