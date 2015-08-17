c33cd4c6000ff2e4db11dc10023d65f50d4bbcf5
ErnestX <ernestxiangjlnq2011@gmail.com>
2015-08-16 22:09:53 -0700

    milestone: now placed nicely. working on highlighting

--- PlansRenderer.js
+++ PlansRenderer.js
@@ -2,7 +2,7 @@ MY_GLOBAL.plansRenderer = {
     thumbnailWidth: 80, 
     thumbnailPadding: 1.5, 
     plansContainer: null, 
-    yPosArray:[], 
+    midYPosArray:[], 
     
     initWithContainer: function(c) {
         MY_GLOBAL.typeChecker.assertIsJQueryObject(c);
@@ -11,35 +11,35 @@ MY_GLOBAL.plansRenderer = {
     }, 
     
     highlightPlanOnScreenAtIndex: function(index) {
-       // unselect all plans on screen
-        this.plansContainer.children().removeClass("selected");
-        this.plansContainer.children().addClass("unselected");
-        // select plan
-        this.plansContainer.children().eq(index).removeClass("unselected");
-        this.plansContainer.children().eq(index).addClass("selected");
+//       // unselect all plans on screen
+//        this.plansContainer.children().removeClass("selected");
+//        this.plansContainer.children().addClass("unselected");
+//        // select plan
+//        this.plansContainer.children().eq(index).removeClass("unselected");
+//        this.plansContainer.children().eq(index).addClass("selected");
     }, 
     
     appendPlan: function(p) {
         MY_GLOBAL.typeChecker.assertIsObjectWithProto(p, MY_GLOBAL.planProto);
         
         var newYPos;
-        if (this.yPosArray.length === 0) {
+        if (this.midYPosArray.length === 0) {
             // the first plan. Put at the center
             newYPos = this.plansContainer.width()/2;
         } else {
             // follow the last entry
-            newYPos = this.yPosArray[this.yPosArray.length - 1] 
+            newYPos = this.midYPosArray[this.midYPosArray.length - 1] 
                 + this.thumbnailWidth 
                 + this.thumbnailPadding;
         }
-        this.yPosArray.push(newYPos);
+        this.midYPosArray.push(newYPos);
         var newPlanJQuery = MY_GLOBAL.thumbnailsRenderer.renderDivFromPlan(p);
-        newPlanJQuery.css('left', newYPos);
+        newPlanJQuery.css('left', this.midYPosToLeftEdgePos(newYPos));
         this.plansContainer.append(newPlanJQuery);
         
         this.centerYPosArrayEntriesWithinPlansContainer();
         this.syncAllThumbnailsYPosWithArray();
-        console.log(this.yPosArray);
+        console.log(this.midYPosArray);
     }, 
     
     prependPlan: function(p) {
@@ -58,21 +58,25 @@ MY_GLOBAL.plansRenderer = {
     
     centerYPosArrayEntriesWithinPlansContainer: function() {
         var sum = 0;
-        for (var i=0; i<this.yPosArray.length; i++) {
-            sum += this.yPosArray[i];
+        for (var i=0; i<this.midYPosArray.length; i++) {
+            sum += this.midYPosArray[i];
         }
         var midY = this.plansContainer.width()/2;
-        var translation = (midY * this.yPosArray.length - sum) / this.yPosArray.length;
+        var translation = (midY * this.midYPosArray.length - sum) / this.midYPosArray.length;
         
-        for (var i=0; i<this.yPosArray.length; i++) {
-            this.yPosArray[i] += translation;
+        for (var i=0; i<this.midYPosArray.length; i++) {
+            this.midYPosArray[i] += translation;
         }
     }, 
     
     syncAllThumbnailsYPosWithArray: function() {
         for(var i=0; i<this.plansContainer.children().length; i++) {
             this.plansContainer.children().eq(i).
-            css('left', this.yPosArray[i].toString() + 'px');
+            css('left', this.midYPosToLeftEdgePos(this.midYPosArray[i]).toString() + 'px');
         }
+    }, 
+    
+    midYPosToLeftEdgePos: function(y) {
+        return y - this.thumbnailWidth/2;
     }
 };
