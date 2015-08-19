033aeef7b19d6ec33e2ecb754f9451cb0c744cb7
ErnestX <ernestxiangjlnq2011@gmail.com>
2015-08-19 00:29:14 -0700

    kinda working. need selectIndex bound check

--- PlansManager.js
+++ PlansManager.js
@@ -18,19 +18,81 @@ MY_GLOBAL.plansManager = {
         }
     }, 
     
-    /*
-    does not move the thumbnailsBar
-    */
     selectIndex: function(planIndex) {
-        if ((planIndex <= this.rangeRight) && (planIndex >= this.rangeLeft)) {
-            MY_GLOBAL.typeChecker.assertIsInteger(planIndex, 'planIndex not int');
-
+        MY_GLOBAL.typeChecker.assertIsInteger(planIndex, 'planIndex not int');
+        
+        // calc target range
+        var targetLeft, targetRight;
+        if (this.maxNumOfLoadedPlans % 2 === 0) { //even
+            // ...(size/2)plans ... selectedPlan ... (size/2 - 1)plans...
+            targetLeft = planIndex - (this.maxNumOfLoadedPlans/2);
+            targetRight = planIndex + (this.maxNumOfLoadedPlans/2 - 1);
+        } else { //odd
+            // ...((size-1)/2)plans ... selectedPlan ... ((size-1)/2)plans...
+            targetLeft = planIndex - (this.maxNumOfLoadedPlans-1)/2;
+            targetRight = planIndex + (this.maxNumOfLoadedPlans-1)/2;
+        }
+//        console.log(targetLeft);
+//        console.log(targetRight);
+        // check whether the target range is within one screen
+        if ((targetLeft <= this.rangeLeft) && (this.rangeLeft <= targetRight) && (targetRight <= this.rangeRight)) {
+            // tl...rl...tr...rr
+            // Step1: remove  tr+1...rr
+            for (var i=targetRight+1; i<=this.rangeRight; i++) {
+                this.tryDeletePlanAtRight();
+            }
+            // Step2: add tl...rl-1
+            for (var i=targetLeft; i<=this.rangeLeft-1; i++) {
+                this.tryAddNewPlanAtLeft();
+            }
+            
+        } else if ((this.rangeLeft <= targetLeft) && (targetLeft <= this.rangeRight) && (this.rangeRight <= targetRight)) {
+            // rl...tl...rr...tr
+            // Step1: remove rl...tl-1
+            for (var i=this.rangeLeft; i<= targetLeft-1; i++) {
+                this.tryDeletePlanAtLeft();
+            }
+            // Step2: add rr+1...tr
+            for (var i=this.rangeRight+1; i<=targetRight; i++) {
+                this.tryAddNewPlanAtRight();
+            }
+        } else if ((targetLeft >= this.rangeLeft) && (targetRight <= this.rangeRight)) {
+            // rl...tl...tr...rr
+            // Step1: remove rl..tl-1
+            for (var i=this.rangeLeft; i<=targetLeft-1; i++) {
+                this.tryDeletePlanAtLeft();
+            }
+            // Step2: remove tr+1...rr
+            for (var i=targetRight+1; i<=this.rangeRight; i++) {
+                this.tryDeletePlanAtRight();
+            }
+        } else if ((this.rangeLeft >= targetLeft) && (this.rangeRight <= targetRight)) {
+            // tl...rl...rr...tr
+            // Step1: add tl...rl-1
+            for (var i=targetLeft; i<=this.rangeLeft-1; i++) {
+                this.tryAddNewPlanAtLeft();
+            }
+            // Step2: add rr+1...tr
+            for (var i=this.rangeRight+1; i<=targetRight; i++) {
+                this.tryAddNewPlanAtRight();
+            }
+        } else if(targetRight <= this.rangeLeft) {
+            // tl...tr...rl...rr
+            this.resetWithRangeLeft(targetLeft);
+            
+        } else if(this.rangeRight <= targetLeft) {
+            // rl...rr...tl...tr
+            this.resetWithRangeLeft(targetLeft);
+        }
+        
+        // Step3: highlight
+//        if ((planIndex <= this.rangeRight) && (planIndex >= this.rangeLeft)) {
             MY_GLOBAL.plansRenderer.unhighlightAllPlansOnScreen();
             var planIndexInOnScreen =  planIndex - this.rangeLeft;
             MY_GLOBAL.plansRenderer.highlightPlanOnScreenAtIndex(planIndexInOnScreen);
             console.log('selected' + planIndex.toString());
             return true;
-        }
+//        }
         return false;
     },
     
@@ -82,6 +144,23 @@ MY_GLOBAL.plansManager = {
         if (this.rangeRight - this.rangeLeft > this.maxNumOfLoadedPlans - 1) { // should not delete when euqal to size
             this.rangeRight--;
             MY_GLOBAL.plansRenderer.removeTailPlan();
+        }
+    }, 
+    
+    resetWithRangeLeft: function(rl) {
+        MY_GLOBAL.assert(rl >= 0);
+        // Step1: delete everything
+        for (var i=this.rangeLeft; i<=this.rangeRight; i++) {
+                this.tryDeletePlanAtRight();
+        }
+        
+        // Step2: reset ranges
+        this.rangeLeft = rl;
+        this.rangeRight = rl-1;
+        
+        // Step3: load
+        for (var i=0; i < this.maxNumOfLoadedPlans; i++) { // try get full size
+            this.tryAddNewPlanAtRight();
         }
     }
 };
